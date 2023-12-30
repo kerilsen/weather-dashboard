3e5e4e77207f06b063d84ccd5c2fcaa79e6b761b
Keri Sen <keri.l.sen@gmail.com>
2023-12-29 21:02:57 -0600

    CSS is ready and search is working but not grabbing the search input yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,51 @@
 $(document).foundation();
-const API_key = "dc9ecf3a826a9bf23eb9e0e35c3b2ca8";
-const lat = "52.1097594";
-const long = "-106.5539789";
 
-const openWeatherEndpoint = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_key}";
+
+const lat = "52.1097594";
+const lat2 = "51.509865";
+const lon = "-106.5539789";
+const lon2 = "0.118092";
+
+const openWeatherEndpoint = `https://api.openweathermap.org/data/3.0/onecall?lat2=${lat}&lon=${lon}&appid=${API_key}`;
+const demoEndpoint = `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=a91de93fc5fcff0a9d6b724ff2c97849`;
+
+const openWeather5Day = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_key}`;
 const currentWeather = document.getElementById('currentWeather');
 const searchButton = document.getElementById('searchCity');
 
 searchButton.addEventListener("click", function () {
-    fetch(openWeatherEndpoint)
+    const API_key = "a91de93fc5fcff0a9d6b724ff2c97849";
+    const cityEl = document.getElementById("inputCity");
+    const city = cityEl.value;
+    console.log("city input is " + city);
+    const geocodingEndpoint = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_key}`;
+    fetch(geocodingEndpoint)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+            console.log(data);
+        })
+
+    console.log("button clicked");
+
+    /* fetch(openWeather5Day)
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (data) {
             // work with all data in here
             console.log(data);
+            const cityName = data.city.name;
+            console.log(cityName);
+            const fiveDay = data.list;
 
-            data.forEach(item => {
+            fiveDay.forEach(item => {
                 // work with each item here
                 console.log(item);
             })
+
         })
-        .catch(error => console.error('Error fetching data from Open Weather API:', error));
+        .catch(error => console.error('Error fetching data from Open Weather API:', error));*/
 });
